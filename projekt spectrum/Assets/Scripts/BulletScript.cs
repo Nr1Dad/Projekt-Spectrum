5e8b548e072f9c1e65fd417f4783549d81c20c35
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-10-10 10:28:00 +0200

    shooting fixed

--- projekt spectrum/Assets/Scripts/BulletScript.cs
+++ projekt spectrum/Assets/Scripts/BulletScript.cs
@@ -36,7 +36,7 @@ public class BulletScript : MonoBehaviour
     private void FixedUpdate(){
        
         //vector3..forward laver fejl med retningn på skud atm.
-        rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Impulse);
+        rb.AddRelativeForce(Vector3.up * speed, ForceMode.Impulse);
 
     }
   
