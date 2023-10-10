12f28a2dd0e7c1241bc56ed4b318b26301f45c30
JohanneNissen <johanne@ornsovej.dk>
2023-10-10 09:29:53 +0200

    Added updateret movement skript

--- projekt spectrum/Assets/Scripts/PlayerController.cs
+++ projekt spectrum/Assets/Scripts/PlayerController.cs
@@ -8,27 +8,51 @@ public class PlayerController : MonoBehaviour
 {
     public int playernumber = 1;
     public float speedDefault = 70;
-    public float turnSpeedDefault = 240;
-    public float turnDampingDefault = 20;
+    public float angularSpeedDefault = 240;
+    public float angularDampingDefault = 20;
+    public float maxDriftSpeed = 0.8f;
 
-    private Rigidbody rb;              
-    private float moveInputValue;
-    private float turnInputValue;            
+    Rigidbody rb;
+    float moveInputValue;
+    float angularInputValue;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        float alphaTS = Mathf.Clamp(rb.velocity.magnitude / speedDefault, 0, 1); // current speed divided by speed and clamped [0;1]
-        float turnSpeed = Mathf.Lerp(turnSpeedDefault, turnDampingDefault, alphaTS); // turnSpeed dependent on current speed
+        // Inputs
 
+
+
+        // Finds and saves the desired angular speed 
+        float currentSpeed = rb.velocity.magnitude;
+        float alphaAngularSpeed = Mathf.Clamp(currentSpeed / speedDefault, 0, 1); // current speed divided by speedDefault and clamped [0;1]
+        float angularSpeed = Mathf.Lerp(angularSpeedDefault, angularDampingDefault, alphaAngularSpeed); // angularSpeed dependent on current speed
+
+        // Drift compensation to not loose controll, when turning
+        float driftCompensation;
+
+        if (Mathf.Sign(angularInputValue) == Mathf.Sign(rb.angularVelocity.y)) // checks if input matches direction
+        {
+            driftCompensation = 1; // Nothing changes
+        }
+        else
+        {
+            float currentAngularSpeed = rb.angularVelocity.magnitude;
+            float alphaDrift = Mathf.Clamp(currentAngularSpeed / maxDriftSpeed, 0, 1);
+            driftCompensation = Mathf.Lerp(1, 2, alphaDrift);
+
+            Debug.Log(driftCompensation);
+        }
+
+        // Add forces to move
         moveInputValue = Input.GetAxis("Vertical"); // [-1;1] back/forward input
         rb.AddRelativeForce(Vector3.forward * moveInputValue * speedDefault);
 
-        turnInputValue = Input.GetAxis("Horizontal"); // [-1;1] left/right input
-        transform.Rotate(Vector3.up * turnInputValue * turnSpeed * Time.deltaTime);
+        angularInputValue = Input.GetAxis("Horizontal"); // [-1;1] left/right input
+        rb.AddRelativeTorque(Vector3.up * angularInputValue * angularSpeed * driftCompensation);
     }
 }
