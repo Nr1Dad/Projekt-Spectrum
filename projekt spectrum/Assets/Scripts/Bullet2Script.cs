d11e53bd2a4098caa3c7383141dd38e533ee73d9
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-11-04 12:23:05 +0100

    bullets + healthslider virker

--- projekt spectrum/Assets/Scripts/Bullet2Script.cs
+++ projekt spectrum/Assets/Scripts/Bullet2Script.cs
@@ -7,8 +7,10 @@ public class Bullet2Script : MonoBehaviour
 {
     public float speed = 10f;
     public float lifetime = 5f;
-
+    public float hitRadius1 = 20f;
     Rigidbody rb;
+    public float minimumDMG = 15;
+    public float maximumDMG = 15;
 
     private void OnCollisionEnter(Collision other)
     {
@@ -24,11 +26,35 @@ public class Bullet2Script : MonoBehaviour
 
         if (other.gameObject.CompareTag("Player1"))
         {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, hitRadius1);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+                if (!targetRigidbody)
+                    continue;
+
+                PlayerHealth targetHealth = targetRigidbody.GetComponent<PlayerHealth>();
+
+                if (!targetHealth) continue;
+
+                float damage = CalculateDamage();
+
+                targetHealth.TakeDamage(damage);
+            }
 
             //Destroy(other.gameObject);
             Destroy(gameObject);
         }
 
+    }
+    private float CalculateDamage()
+    {
+
+
+        float damage = maximumDMG;
+
+        return damage;
     }
 
 
