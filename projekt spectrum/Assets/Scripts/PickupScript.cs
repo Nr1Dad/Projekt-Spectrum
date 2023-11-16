074b6077afc1b4189113d87daef5d631c5876d29
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-11-16 13:47:44 +0100

    pickup og astroide mat

---
+++ projekt spectrum/Assets/Scripts/PickupScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(2, 1, 1));
+    }
+}
