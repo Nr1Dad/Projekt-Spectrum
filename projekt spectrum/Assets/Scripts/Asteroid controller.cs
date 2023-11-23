392a13bac40b024add2a1bf7ff2b943e6ab6ee2c
JohanneNissen <johanne@ornsovej.dk>
2023-11-23 09:36:29 +0100

    Only p2 attack hit
    Kun p2 skud kan skade asteroider

--- projekt spectrum/Assets/Scripts/Asteroid controller.cs
+++ projekt spectrum/Assets/Scripts/Asteroid controller.cs
@@ -8,6 +8,7 @@ public class Asteroidcontroller : MonoBehaviour
     Rigidbody rb;
     public float KnockbackStreangth;
     public float AsteroidHealth;
+    public GameObject Collectable;
 
 
     // Start is called before the first frame update
@@ -18,18 +19,23 @@ public class Asteroidcontroller : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)   //detect collision
     {
-        if (other.gameObject.CompareTag("Bullet"))   //detect if tag on collided object is "Bullet"
+        if (other.gameObject.CompareTag("Bullet2"))   //detect if tag on collided object is "Bullet"
         {
             rb.AddForce(Vector3.up * KnockbackStreangth, ForceMode.Impulse);   //Add force equal depending on set knockback streangth. Is also affected by object mass.
             if (AsteroidHealth <= 10)
-            {
-                Debug.Log("Hit");
+            {               
                 AsteroidHealth -- ;
+                Instantiate(Collectable, Vector3.back, Quaternion.identity);
             }
             if (AsteroidHealth < 1)
             {
                 Destroy(gameObject);
+                Instantiate(Collectable, gameObject.transform);
             }
+        }
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            rb.AddForce(Vector3.up * KnockbackStreangth, ForceMode.Impulse);
         }
 
         if ( other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))   //detect if tag on collided object is either "Player1" or "Player2"
